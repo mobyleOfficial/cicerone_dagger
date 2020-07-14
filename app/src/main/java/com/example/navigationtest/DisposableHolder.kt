47c84b2afd2884dae2d44e3cde214f6d2c7a91f2
Bruno Bacelar Abe <messengerabe@hotmail.com>
2020-07-14 11:17:29 -0300

    Ading comments
    Adicionando comentários ao código.

--- app/src/main/java/com/example/navigationtest/DisposableHolder.kt
+++ app/src/main/java/com/example/navigationtest/DisposableHolder.kt
@@ -3,6 +3,7 @@ package com.example.navigationtest
 import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
+// Bag para dar dispose nos meus subscribes!
 interface DisposableHolder {
     val disposables: CompositeDisposable
 
